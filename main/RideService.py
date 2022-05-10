54dabb062786834c788fb45958185d25044c4504
z <z.y.j.1379@gmail.com>
2022-05-10 22:24:31 +0430

    change status of bike and customer when riding, and create a Ride class in RideService.py

--- main/RideService.py
+++ main/RideService.py
@@ -1,10 +1,12 @@
 from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy import Date, func
 
 from main.app import app, engine
 from flask import request, jsonify
 from sqlalchemy.orm import Session
 
-from main.models import Customer, Bike
+from main.haversine import haversine
+from main.models import Customer, Bike, Ride
 
 
 # Protect a route with jwt_required, which will kick out requests
@@ -23,11 +25,20 @@ def getRide():
     session = Session(engine)
     bike_obj = session.query(Bike).get({"id": bike_id})
     if bike_obj is None:
+        session.close()
         return jsonify({"msg": "Bike not found"}), 401
     if bike_obj.status != "available":
+        session.close()
         return jsonify({"msg": "Bike is not available"}), 401
 
     user_obj = session.query(Customer).get({"id": current_user})
     if haversine(bike_obj.loc_x, bike_obj.loc_y, user_obj.loc_x, user_obj.loc_y) < 100:
+        user_obj.status = "riding"
+        bike_obj.status = "busy"
+        ride = Ride(customer_id=user_obj.id, bike_id=bike_obj.id, status="ongoing", date=func.current_date())
+        session.add(ride)
+        session.commit()
+        session.close()
         return jsonify({"msg": "Bike is for you"}), 200
+    session.close()
     return jsonify({"msg": "Bike is far from you"}), 401
