4734fa8a4a33dcd1ca5681c476fcba6fa189fa7e
z <z.y.j.1379@gmail.com>
2022-05-11 00:38:58 +0430

    add login and signup ui

--- main/app.py
+++ main/app.py
@@ -1,8 +1,11 @@
 import datetime
 
-from flask import Flask
+import flask
+import form as form
+from flask import Flask, render_template
 from flask import jsonify
 from flask import request
+from flask import Flask, redirect, url_for
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
@@ -13,7 +16,10 @@ from sqlalchemy.orm import Session
 
 from main import models
 from main.haversine import haversine
-from main.models import Customer, Bike
+from main.models import Customer, Bike, Ride
+
+from sqlalchemy import Date, func
+import requests
 
 Base = models.Base
 
@@ -40,6 +46,153 @@ def make_bikes():
     session.close()
 
 
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('Login') == 'login':
+            return redirect(url_for('login_view'))
+        elif request.form.get('Signup') == 'signup':
+            return redirect(url_for('signup_view'))
+        else:
+            return "Error"
+    elif request.method == 'GET':
+        return render_template('index.html', form=form)
+
+    return render_template("index.html")
+
+
+@app.route('/login_view', methods=['GET', 'POST'])
+def login_view():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        dictToSend = {'username': username, "password": password}
+        res = requests.post('http://localhost:5000/login', json=dictToSend)
+        return res.text
+
+
+@app.route('/signup_view', methods=['GET', 'POST'])
+def signup_view():
+    if request.method == 'GET':
+        return render_template("signup.html")
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        dictToSend = {'username': username, "password": password}
+        res = requests.post('http://localhost:5000//signup', json=dictToSend)
+        return res.text
+
+
+# --------------------------------------------------------------------------------
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    session = Session(engine)
+    user_obj = session.query(Customer).get({"username": username, "password": password})
+    session.close()
+    if user_obj is None:
+        return jsonify({"msg": "Username Or Password is incorrect"}), 401
+
+    access_token = create_access_token(identity=user_obj.id)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # try:
+    session = Session(engine)
+    user = Customer(username=username, password=password)
+    session.add(user)
+    session.commit()
+    session.close()
+    user_id = user.id
+    print(user_id)
+    # except:
+    #     print("problem")
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=user_id)
+    return jsonify(access_token=access_token)
+# --------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/get_ride", methods=["GET"])
+@jwt_required()
+def getRide():
+    current_user = get_jwt_identity()
+    bike_id = request.json.get("bike_id", None)
+    try:
+        bike_id = int(bike_id)
+    except:
+        print("problem")
+        return jsonify({"msg": "Format of ID is not correct"}), 401
+
+    session = Session(engine)
+    bike_obj = session.query(Bike).get({"id": bike_id})
+    if bike_obj is None:
+        session.close()
+        return jsonify({"msg": "Bike not found"}), 401
+    if bike_obj.status != "available":
+        session.close()
+        return jsonify({"msg": "Bike is not available"}), 401
+
+    user_obj = session.query(Customer).get({"id": current_user})
+    if haversine(bike_obj.loc_x, bike_obj.loc_y, user_obj.loc_x, user_obj.loc_y) < 100:
+        user_obj.status = "riding"
+        bike_obj.status = "busy"
+        ride = Ride(customer_id=user_obj.id, bike_id=bike_obj.id, status="ongoing", date=func.current_date())
+        session.add(ride)
+        session.commit()
+        session.close()
+        return jsonify({"msg": "ride_id:" + str(ride.id)}), 200
+    session.close()
+    return jsonify({"msg": "Bike is far from you"}), 401
+
+
+@app.route("/end_ride", methods=["GET"])
+@jwt_required()
+def endRide():
+    current_user = get_jwt_identity()
+    ride_id = request.json.get("ride_id", None)
+    bike_loc_x = request.json.get("loc_x", None)
+    bike_loc_y = request.json.get("loc_y", None)
+    try:
+        ride_id = int(ride_id)
+    except:
+        print("problem")
+        return jsonify({"msg": "Format of ID is not correct"}), 401
+    session = Session(engine)
+    ride_obj = session.query(Ride).get({"id": ride_id})
+    if ride_obj.customer_id != current_user:
+        session.close()
+        return jsonify({"msg": "You are not the rider"}), 401
+
+    user_obj = session.query(Customer).get({"id": current_user})
+    bike_obj = session.query(Bike).get({"id": ride_obj.bike_id})
+
+    user_obj.status = "free"
+    bike_obj.status = "available"
+    ride_obj.status = "finished"
+    ride_obj.distance = haversine(bike_obj.loc_x, bike_obj.loc_y, bike_loc_x, bike_loc_y)
+    session.commit()
+    session.close()
+    return jsonify({"msg": "end of riding"}), 200
+
+
+# --------------------------------------------------------------------------------
+
+
 if __name__ == "__main__":
-    make_bikes()
+    # make_bikes()
     app.run()
